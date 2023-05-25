a04a404bf84bdb594bd86526bef285f3a8f5bac2
Skyfe24 <fabio.erase@gmail.com>
2023-05-25 16:02:07 +0200

    JS completed

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('JS OK');
+const userName = prompt('Qual è il tuo nome?' , 'Fabio');
+console.log('JS OK');
+const userSurame = prompt('Qual è il tuo cognome?' , 'Campagna');
+console.log('JS OK');
+const userFavoritecolor = prompt('Qual è il tuo colore preferito?' , 'Azzurro');
+console.log('JS OK');
+
+const numberTwentyone = 21;
+console.log('JS OK');
+
+document.getElementById('userPsw').innerText = userName + userSurame + userFavoritecolor + numberTwentyone
